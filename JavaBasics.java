a2da62aab8b7f06bd12ea4902e31c2fa7ff9f214
Rajneesh Chaubey <pinea8888@gmail.com>
2026-02-21 19:20:41 +0530

    Day 1: Java Basics - Hello World

---
+++ JavaBasics.java
@@ -0,0 +1,23 @@
+// public class JavaBasics {
+//     public static void main(String args[]) {
+//         System.out.print("Hello World");
+//     }
+// }
+
+// public class JavaBasics {
+//     public static void main(String args[]) {
+//         System.out.print("Hello World");
+//         System.out.print("Hello World");
+//         System.out.print("Hello World");
+//     }
+// }
+
+// for next line we use println 
+
+public class JavaBasics {
+    public static void main(String args[]) {
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}

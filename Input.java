87083bc88a97964b1f57d45390c83ee58a3f8921
Rajneesh Chaubey <pinea8888@gmail.com>
2026-03-21 03:10:33 +0530

    Input from users

---
+++ Input.java
@@ -0,0 +1,21 @@
+// We use Scanner for taking input from the users
+
+import java.util.*;
+public class input {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        String input = sc.next();
+        System.out.println(input);
+    }
+}
+
+// import java.util.*;
+// public class Input {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int sum = a + b;
+//         System.out.println(sum);
+//     }
+// }

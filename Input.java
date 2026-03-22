db91ce85a264863cf24b58839eb08db500148415
Rajneesh Chaubey <pinea8888@gmail.com>
2026-03-22 20:29:23 +0530

    use of nextLine() in input

--- Input.java
+++ Input.java
@@ -1,13 +1,33 @@
 // We use Scanner for taking input from the users
 
+// import java.util.*;
+// public class input {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         String input = sc.next();
+//         System.out.println(input);
+//     }
+// }
+
+
+// next() sirf space tak ya ek word hi leta hai jaise ki pine.
+// agar mujhe ek words ye jyda (puri line, sentence , para) ke liye nextLine() use krna hoga.
+// next() He is a goog boy  O/P - He
+// nextLine() He is a good boy O/P - He is good boy
+
+
 import java.util.*;
 public class input {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-        String input = sc.next();
-        System.out.println(input);
+        String a = sc.nextLine();
+        System.out.println(a);
     }
 }
+
+
+
+// Input from users (sum of a & b)
 
 // import java.util.*;
 // public class Input {

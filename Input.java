62daf2cc503db7ee63e749bbd71387871f208cec
Rajneesh Chaubey <pinea8888@gmail.com>
2026-03-23 00:56:37 +0530

    Input from user(Product of a & b)

--- Input.java
+++ Input.java
@@ -43,13 +43,29 @@
 
 // Input from users (subtraction of a & b)
 
+// import java.util.*;
+// public class input {
+//     public static void main(String args[]) {
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int sub = a-b;
+//         System.out.println(sub);
+//     }
+// }
+
+
+
+// Input from user(Product of a & b)
+
+
 import java.util.*;
 public class input {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
-        int sub = a-b;
-        System.out.println(sub);
+        int product = a*b;
+        System.out.println(product);
     }
 }

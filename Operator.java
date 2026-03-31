781651100006007ffa03bbf44639074a240c5482
Rajneesh Chaubey <pinea8888@gmail.com>
2026-03-31 17:28:18 +0530

    Relational operators in java

--- Operator.java
+++ Operator.java
@@ -1,15 +1,50 @@
-import java.util.*;
+// import java.util.*;
 
-public class Operator {
+// public class Operator {
+//     public static void main(String args[]) {
+//         int A = 20 ;
+//         int B = 15 ;
+//         System.out.println(A+B);
+//         System.out.println("add = " + (A+B));
+//         System.out.println("Sub = " + (A-B));
+//         System.out.println("Multiply = " + (A*B));
+//         System.out.println("Divide = " + (A/B));
+//         System.out.println("Mod = " + (A%B));
+
+//     }
+// }
+
+
+// o/p -
+// 35
+// add = 35
+// Sub = 5
+// Multiply = 300
+// Divide = 1
+// Mod = 5
+
+
+ // Relational Operators 
+//  ==
+//  !=
+//  >
+//  <
+//  >=
+//  <=
+
+// public class operator {
+//     public static void main(String args[]) {
+//         int A = 10 ;
+//         int B = 5 ;
+//         System.out.println((A==B));
+//     }
+// }
+
+public class operator {
     public static void main(String args[]) {
-        int A = 20 ;
-        int B = 15 ;
-        System.out.println(A+B);
-        System.out.println("add = " + (A+B));
-        System.out.println("Sub = " + (A-B));
-        System.out.println("Multiply = " + (A*B));
-        System.out.println("Divide = " + (A/B));
-        System.out.println("Mod = " + (A%B));
-
+        int A = 10 ;
+        int B = 10 ;
+        System.out.println((A==B));
     }
 }
+

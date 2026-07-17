cf5892410f08506cc9c2ad5c4eebfe07cf69e940
Rajneesh Chaubey <pinea8888@gmail.com>
2026-07-18 02:11:15 +0530

    inverted and char pattern

--- Pattern.java
+++ Pattern.java
@@ -23,22 +23,122 @@
 
 
 
-import java.util.*;
 
-public class Pattern {
-    public static void main(String args[]) {
-        
-        // Outer loop for rows
-        for (int i = 5; i >= 1; i--) {
-            // Inner loop prints stars equal to the current line number
-            for (int j = 5; j >= i; j--) {
-            
-                System.out.print(j); 
-            }
-            // Moves to the next line after printing all stars in the row
-            System.out.println(); 
-        }
-        
-        
-    }
-}
+// print start
+// *
+// **
+// ***
+// ****
+
+// public class pattern {
+//     public static void main(String args[]) {
+//         for(int i = 1; i<= 4; i++) {
+//             for(int j = 1; j <= i; j++) {
+//                  System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+      
+
+//     }
+// }
+
+
+
+
+// for printing this 
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+// *******
+// ********
+// *********
+// **********
+
+// public class pattern {
+//     public static void main(String args[]) {
+//         for(int i = 1; i<= 10; i++) {
+//             for(int j = 1; j <= i; j++) {
+//                  System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+      
+
+//     }
+// }
+
+
+
+// //inverted star 
+
+// public class pattern {
+//     public static void main(String args[]) {
+//         int n = 10;
+//         for(int i = 1; i<=n; i++) {
+//             for(int j = 1; j <=n-i+1 ; j++) {
+//                  System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// o/p
+// *********
+// ********
+// *******
+// ******
+// *****
+// ****
+// ***
+// **
+// *
+
+
+
+
+// // 1
+// // 12
+// // 123
+// // 1234
+
+// public class pattern {
+//     public static void main(String args[]) {
+//         int n = 4;
+//         for(int i = 1; i<= n; i++) {
+//             for(int j = 1; j <= i; j++) {
+//                  System.out.print(j);
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+// public class pattern {
+//     public static void main(String args[]) {
+//         int n = 4;
+//         char ch = 'A';
+//         for(int i = 1; i<= n; i++) {
+//             for(int j = 1; j <= i; j++) {
+//                  System.out.print(ch);
+//                  ch++;
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+// o/p - 
+// A
+// BC
+// DEF
+// GHIJ
+
+
+
+
